4c06b924847f5e133e77814942286dc10634608a
gempitadini <gempitadini05@gmail.com>
2025-05-19 16:59:22 +0700

    Modif Percobaan 2

--- SLLMain11.java
+++ SLLMain11.java
@@ -1,29 +1,27 @@
-import java.util.Scanner;
 public class SLLMain11 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         SingleLinkedList11 sll = new SingleLinkedList11();
+
+        Mahasiswa11 mhs4 = new Mahasiswa11("244107060075", "Gempita", "2A", 3.9);
+        Mahasiswa11 mhs3 = new Mahasiswa11("244107060056", "Fitri", "2A", 3.8);
+        Mahasiswa11 mhs1 = new Mahasiswa11("244107060023", "Nur", "2A", 3.7);
+        Mahasiswa11 mhs2 = new Mahasiswa11("244107060123", "Dini", "2A", 3.6);
+
+        sll.addFirst(mhs4);
+        sll.addLast(mhs1);
+        sll.addLast(mhs2);
+        sll.addLast(mhs3);
+
+        System.out.println("Data indeks 1: ");
+        sll.getData(1);
+
+        System.out.println("Data mahasiswa an Dini berada pada indeks: " + sll.indexOf("Dini"));
+        System.out.println();
+
+        sll.removeFirst();
+        sll.removeLast();
         sll.print();
-
-        System.out.print("Masukkan Jumlah Mahasiswa: ");
-        int jumlahMahasiswa = sc.nextInt();
-        sc.nextLine();
-
-        for (int i = 0; i < jumlahMahasiswa; i++) {
-            System.out.println("\nMasukkan data mahasiswa ke-" + (i+1));
-            System.out.print("NIM: ");
-            String nim = sc.nextLine();
-            System.out.print("Nama: ");
-            String nama = sc.nextLine();
-            System.out.print("Kelas: ");
-            String kelas = sc.nextLine();
-            System.out.print("IPK: ");
-            double ipk = Double.parseDouble(sc.nextLine());
-
-            Mahasiswa11 mhs = new Mahasiswa11(nim, nama, kelas, ipk);
-            sll.addLast(mhs);
-            sll.print();
-        }
-        sc.close();
+        sll.removeAt(0);
+        sll.print();
     }
 }
