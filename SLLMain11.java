f302ff13dfa8f86dacf5ce3ceb855c3cf4e5ee12
gempitadini <gempitadini05@gmail.com>
2025-05-19 16:39:57 +0700

    Modif Percobaan 1

--- SLLMain11.java
+++ SLLMain11.java
@@ -1,19 +1,29 @@
+import java.util.Scanner;
 public class SLLMain11 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         SingleLinkedList11 sll = new SingleLinkedList11();
+        sll.print();
 
-        Mahasiswa11 mhs1 = new Mahasiswa11("244107060083", "Gempita", "2A", 3.9);
-        Mahasiswa11 mhs2 = new Mahasiswa11("244107060084", "Fitri", "2A", 3.8);
-        Mahasiswa11 mhs3 = new Mahasiswa11("244107060085", "Nur", "2A", 3.7);
-        Mahasiswa11 mhs4 = new Mahasiswa11("244107060086", "Dini", "2A", 3.6);
+        System.out.print("Masukkan Jumlah Mahasiswa: ");
+        int jumlahMahasiswa = sc.nextInt();
+        sc.nextLine();
 
-        sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Dini", mhs3);  
-        sll.insertAt(2, mhs2);
-        sll.print();
+        for (int i = 0; i < jumlahMahasiswa; i++) {
+            System.out.println("\nMasukkan data mahasiswa ke-" + (i+1));
+            System.out.print("NIM: ");
+            String nim = sc.nextLine();
+            System.out.print("Nama: ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas: ");
+            String kelas = sc.nextLine();
+            System.out.print("IPK: ");
+            double ipk = Double.parseDouble(sc.nextLine());
+
+            Mahasiswa11 mhs = new Mahasiswa11(nim, nama, kelas, ipk);
+            sll.addLast(mhs);
+            sll.print();
+        }
+        sc.close();
     }
 }
